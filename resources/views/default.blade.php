ac1520b6a43f0fa4b69a39bc15fde68660c53528
Miguel Angel Lopez Ariza <lopezarizamiguel@gmail.com>
2023-12-13 22:13:38 -0500

    agregando google analytics

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -52,6 +52,16 @@
             @endif
         @endforeach
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('larecipe.google-analytics.id') }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '{{ config('larecipe.google-analytics.id') }}');
+        </script>
+
     </head>
     <body>
         <div id="app" v-cloak>
